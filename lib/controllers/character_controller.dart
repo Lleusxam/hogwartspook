59c2a3acf3edfea9ddfe8e9f00284fec1210a281
lleusxam <blkleusxam@hotmail.com>
2024-08-11 19:24:05 -0300

    Add: Adição da barra de pesquisa de personagens na tela Home

--- lib/controllers/character_controller.dart
+++ lib/controllers/character_controller.dart
@@ -1,16 +1,45 @@
 import 'dart:convert';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import '../models/character_model.dart';
 
-Future<List<Character>> fetchCharacters() async {
-  final response = await http.get(Uri.parse('https://hp-api.onrender.com/api/characters'));
+class CharacterController extends GetxController {
+  var characters = <Character>[].obs;
+  var filteredCharacters = <Character>[].obs;
+  var isLoading = true.obs;
 
-  if (response.statusCode == 200) {
-    List<dynamic> jsonData = json.decode(response.body);
-    return jsonData.map((json) => Character.fromJson(json)).toList();
-  }
-  else {
-    throw Exception("Failed to load characters.");
+  @override
+  void onInit() {
+    super.onInit();
+    fetchCharacters();
   }
 
+  Future<void> fetchCharacters() async {
+    try {
+      isLoading(true);
+      final response = await http.get(Uri.parse('https://hp-api.onrender.com/api/characters'));
+      if (response.statusCode == 200) {
+        List<dynamic> jsonData = json.decode(response.body);
+        characters.value = jsonData.map((json) => Character.fromJson(json)).toList();
+        filteredCharacters.value = characters; // Inicializa com todos os personagens
+      } else {
+        throw Exception("Failed to load characters.");
+      }
+    } catch (e) {
+      // Handle error
+      print(e);
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  void filterCharacters(String query) {
+    if (query.isEmpty) {
+      filteredCharacters.value = characters;
+    } else {
+      filteredCharacters.value = characters
+          .where((character) => character.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    }
+  }
 }
--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:projetopoo/models/character_model.dart';
 import 'package:projetopoo/controllers/character_controller.dart';
 import 'package:projetopoo/views/screens/character.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
+    final CharacterController controller = Get.put(CharacterController());
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Stack(
@@ -21,74 +24,97 @@ class Home extends StatelessWidget {
               ),
             ),
           ),
-          FutureBuilder<List<Character>>(
-            future: fetchCharacters(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text("Error: ${snapshot.error}"));
-              } else {
-                return ListView.builder(
-                  padding: const EdgeInsets.all(8.0),
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    Character character = snapshot.data![index];
-                    return Card(
-                      color: Colors.transparent,
-                      margin: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: ListTile(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => CharacterDetailScreen(character: character),
-                            ),
-                          );
-                        },
-                        contentPadding: const EdgeInsets.all(10.0),
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(50.0),
-                          child: character.image.isNotEmpty
-                              ? Image.network(
-                                  character.image,
-                                  width: 50.0,
-                                  height: 50.0,
-                                  fit: BoxFit.cover,
-                                )
-                              : Image.asset(
-                                  'images/default-photo.png',
-                                  width: 50.0,
-                                  height: 50.0,
-                                  fit: BoxFit.cover,
-                                ),
-                        ),
-                        title: Text(
-                          character.name,
-                          style: const TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white, // Cor do texto ajustada para ser visível no fundo
-                          ),
-                        ),
-                        subtitle: Text(
-                          character.house,
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.grey[400], // Cor do subtítulo ajustada
-                          ),
-                        ),
-                        trailing: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.grey,
-                          size: 20.0,
-                        ),
-                      ),
-                    );
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search characters...",
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.2),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  onChanged: (query) {
+                    controller.filterCharacters(query);
                   },
-                );
-              }
-            },
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              Expanded(
+                child: Obx(() {
+                  if (controller.isLoading.value) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (controller.filteredCharacters.isEmpty) {
+                    return const Center(child: Text("No characters found.", style: TextStyle(color: Colors.white)));
+                  } else {
+                    return ListView.builder(
+                      padding: const EdgeInsets.all(8.0),
+                      itemCount: controller.filteredCharacters.length,
+                      itemBuilder: (context, index) {
+                        Character character = controller.filteredCharacters[index];
+                        return Card(
+                          color: Colors.transparent,
+                          margin: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: ListTile(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CharacterDetailScreen(character: character),
+                                ),
+                              );
+                            },
+                            contentPadding: const EdgeInsets.all(10.0),
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(50.0),
+                              child: character.image.isNotEmpty
+                                  ? Image.network(
+                                      character.image,
+                                      width: 50.0,
+                                      height: 50.0,
+                                      fit: BoxFit.cover,
+                                    )
+                                  : Image.asset(
+                                      'images/default-photo.png',
+                                      width: 50.0,
+                                      height: 50.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                            ),
+                            title: Text(
+                              character.name,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            subtitle: Text(
+                              character.house,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.grey[400],
+                              ),
+                            ),
+                            trailing: const Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.grey,
+                              size: 20.0,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                }),
+              ),
+            ],
           ),
         ],
       ),
